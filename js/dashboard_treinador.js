f536ca0fa28c3adb810a9ba6073e819dcab7e937
Pedro Bezerra <pedroaugustobezerra@gmail.com>
2026-07-01 13:33:11 -0300

    Adicionando Funcionalidade Treinador

--- js/dashboard_treinador.js
+++ js/dashboard_treinador.js
@@ -13,7 +13,6 @@ async function verificarLogin() {
     }
 
     return true;
-
 }
 
 async function carregarTreinador() {
@@ -22,32 +21,118 @@ async function carregarTreinador() {
 
     const id = session.session.user.id;
 
-    const { data } = await db
+    const { data, error } = await db
         .from("profiles")
         .select("*")
         .eq("id", id)
         .single();
 
+    if (error) {
+
+        console.error(error);
+
+        return;
+
+    }
+
     document.getElementById("nomeTreinador").innerHTML = `
         Bem-vindo,<br>
         ${data.nome} 👋
     `;
+}
+
+async function carregarUsuarios() {
+
+    const { data, error } = await db
+        .from("profiles")
+        .select("id, nome")
+        .eq("type", "aluno");
+
+    if (error) {
+
+        console.error(error);
+
+        return;
+
+    }
+
+    const select = document.getElementById("listaUsuarios");
+
+    select.innerHTML = "";
+
+    data.forEach(aluno => {
+
+        select.innerHTML += `
+            <option value="${aluno.id}">
+                ${aluno.nome}
+            </option>
+        `;
+
+    });
 
 }
 
-async function logout(){
+function abrirModalAluno() {
+
+    document.getElementById("modalAluno").style.display = "flex";
+
+    carregarUsuarios();
+
+}
+
+function fecharModal() {
+
+    document.getElementById("modalAluno").style.display = "none";
+
+}
+
+async function adicionarAluno() {
+
+    const alunoId =
+        document.getElementById("listaUsuarios").value;
+
+    const { data: session } =
+        await db.auth.getSession();
+
+    const treinadorId = session.session.user.id;
+
+    const { error } = await db
+        .from("treinador_alunos")
+        .insert({
+
+            treinador_id: treinadorId,
+
+            aluno_id: alunoId
+
+        });
+
+    if (error) {
+
+        alert(error.message);
+
+        return;
+
+    }
+
+    alert("Aluno adicionado com sucesso!");
+
+    fecharModal();
+
+}
+
+async function logout() {
 
     await db.auth.signOut();
 
-    window.location.href="login.html";
+    window.location.href = "login.html";
 
 }
 
-async function iniciar(){
+async function iniciar() {
 
-    const ok=await verificarLogin();
+    const ok = await verificarLogin();
 
-    if(!ok)return;
+    if (!ok) return;
 
     await carregarTreinador();
 
